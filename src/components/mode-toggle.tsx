365d9bb4b7c1069adbcccfec8b52a89f6b8eb825
Guany <1084072690@qq.com>
2025-08-06 13:00:36 +0800

    feat: 添加 @radix-ui/react-slot 依赖并重构按钮组件

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@/components/theme-provider'
+import { Button } from './ui/button'
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
@@ -50,10 +51,32 @@ export function ModeToggle() {
   }
 
   return (
-    <button
-      className="icon-btn i-carbon-sun dark:i-carbon-moon"
+    <Button
+      variant="secondary"
+      size="icon"
+      className="size-8"
       title="切换深色模式"
       onClick={toggleDark}
-    ></button>
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="size-4.5"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M12 3l0 18" />
+        <path d="M12 9l4.65 -4.65" />
+        <path d="M12 14.3l7.37 -7.37" />
+        <path d="M12 19.6l8.85 -8.85" />
+      </svg>
+    </Button>
   )
 }
