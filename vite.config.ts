189f935270715be4fdfc1f43d74017f1b0a66cb5
Guany <1084072690@qq.com>
2025-11-15 18:58:16 +0800

    更新多个依赖版本，包括 @types/react、@vitejs/plugin-react、prettier-plugin-tailwindcss 和 typescript，同时调整 Node.js 版本要求，并删除 pnpm-workspace.yaml 文件。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-oxc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
